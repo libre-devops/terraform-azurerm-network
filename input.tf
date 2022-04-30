a69959dcb43b08766cc46082a54aed03b7c5b303
Craig Thacker <craigthackerx@gmail.com>
2022-05-01 00:52:41 +0100

    Update module

--- input.tf
+++ input.tf
@@ -1,3 +1,30 @@
+variable "address_space" {
+  type        = list(string)
+  description = "The address space that is used by the virtual network."
+  default     = ["10.0.0.0/16"]
+}
+
+# If no values specified, this defaults to Azure DNS
+
+variable "dns_servers" {
+  description = "The DNS servers to be used with vNet."
+  type        = list(string)
+  default     = []
+}
+
+variable "location" {
+  description = "The location for this resource to be put in"
+  type        = string
+}
+
+variable "nsg_ids" {
+  description = "A map of subnet name to Network Security Group IDs"
+  type        = map(string)
+
+  default = {
+  }
+}
+
 variable "rg_name" {
   description = "The name of the resource group, this module does not create a resource group, it is expecting the value of a resource group already exists"
   type        = string
@@ -7,45 +34,15 @@ variable "rg_name" {
   }
 }
 
-variable "location" {
-  description = "The location for this resource to be put in"
-  type        = string
+variable "route_tables_ids" {
+  description = "A map of subnet name to Route table ids"
+  type        = map(string)
+  default     = {}
 }
 
-variable "vnet_name" {
-  description = "Name of the vnet to create"
-  type        = string
-  default     = "acctvnet"
-}
-
-variable "address_space" {
-  type        = list(string)
-  description = "The address space that is used by the virtual network."
-  default     = ["10.0.0.0/16"]
-}
-
-# If no values specified, this defaults to Azure DNS
-variable "dns_servers" {
-  description = "The DNS servers to be used with vNet."
-  type        = list(string)
-  default     = []
-}
-
-variable "subnet_prefixes" {
-  description = "The address prefix to use for the subnet."
-  type        = list(string)
-  default     = ["10.0.1.0/24"]
-}
-
-variable "subnet_names" {
-  description = "A list of public subnets inside the vNet."
-  type        = list(string)
-  default     = ["subnet1", "subnet2", "subnet3"]
-}
-
-variable "subnet_service_endpoints" {
-  description = "A map of subnet name to service endpoints to add to the subnet."
-  type        = map(any)
+variable "subnet_delegation" {
+  description = "A map of subnet name to delegation block on the subnet"
+  type        = map(map(any))
   default     = {}
 }
 
@@ -61,23 +58,21 @@ variable "subnet_enforce_private_link_service_network_policies" {
   default     = {}
 }
 
-variable "subnet_delegation" {
-  description = "A map of subnet name to delegation block on the subnet"
-  type        = map(map(any))
-  default     = {}
+variable "subnet_names" {
+  description = "A list of public subnets inside the vNet."
+  type        = list(string)
+  default     = ["subnet1", "subnet2", "subnet3"]
 }
 
-variable "nsg_ids" {
-  description = "A map of subnet name to Network Security Group IDs"
-  type        = map(string)
-
-  default = {
-  }
+variable "subnet_prefixes" {
+  description = "The address prefix to use for the subnet."
+  type        = list(string)
+  default     = ["10.0.1.0/24"]
 }
 
-variable "route_tables_ids" {
-  description = "A map of subnet name to Route table ids"
-  type        = map(string)
+variable "subnet_service_endpoints" {
+  description = "A map of subnet name to service endpoints to add to the subnet."
+  type        = map(any)
   default     = {}
 }
 
@@ -94,4 +89,10 @@ variable "vnet_location" {
   description = "The location of the vnet to create. Defaults to the location of the resource group."
   type        = string
   default     = null
+}
+
+variable "vnet_name" {
+  description = "Name of the vnet to create"
+  type        = string
+  default     = "acctvnet"
 }
--- output.tf
+++ output.tf
@@ -1,31 +1,9 @@
-output "vnet_id" {
-  description = "The id of the newly created vNet"
-  value       = azurerm_virtual_network.vnet.id
-}
-
-output "vnet_name" {
-  description = "The Name of the newly created vNet"
-  value       = azurerm_virtual_network.vnet.name
-}
-
-output "vnet_location" {
-  description = "The location of the newly created vNet"
-  value       = azurerm_virtual_network.vnet.location
-}
-
-output "vnet_address_space" {
-  description = "The address space of the newly created vNet"
-  value       = azurerm_virtual_network.vnet.address_space
-}
-
-output "vnet_rg_name" {
-  description = "The resource group name which the VNet is in"
-  value       = azurerm_virtual_network.vnet.resource_group_name
-}
-
-output "vnet_subnets" {
-  description = "The ids of subnets created inside the newly created vNet"
-  value       = azurerm_subnet.subnet.*.id
+output "subnets_ids" {
+  value = {
+    for index, subnet in azurerm_subnet.subnet :
+    subnet.name => subnet.id
+  }
+  description = "The ids of the subnets created"
 }
 
 output "subnets_names" {
@@ -36,10 +14,32 @@ output "subnets_names" {
   description = "The name of the subnets created"
 }
 
-output "subnets_ids" {
-  value = {
-    for index, subnet in azurerm_subnet.subnet :
-    subnet.name => subnet.id
-  }
-  description = "The ids of the subnets created"
+output "vnet_address_space" {
+  description = "The address space of the newly created vNet"
+  value       = azurerm_virtual_network.vnet.address_space
+}
+
+output "vnet_id" {
+  description = "The id of the newly created vNet"
+  value       = azurerm_virtual_network.vnet.id
+}
+
+output "vnet_location" {
+  description = "The location of the newly created vNet"
+  value       = azurerm_virtual_network.vnet.location
+}
+
+output "vnet_name" {
+  description = "The Name of the newly created vNet"
+  value       = azurerm_virtual_network.vnet.name
+}
+
+output "vnet_rg_name" {
+  description = "The resource group name which the VNet is in"
+  value       = azurerm_virtual_network.vnet.resource_group_name
+}
+
+output "vnet_subnets" {
+  description = "The ids of subnets created inside the newly created vNet"
+  value       = azurerm_subnet.subnet.*.id
 }
