8e5745e06a469eb9aff29826ff7d27da4844f9bf
Craig Thacker <craigthackerx@gmail.com>
2022-10-16 04:31:49 +0100

    Update module

--- terraform/build.tf
+++ terraform/build.tf
@@ -19,11 +19,27 @@ module "network" {
   vnet_location = module.network.vnet_location
 
   address_space   = ["10.0.0.0/16"]
-  subnet_prefixes = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  subnet_prefixes = ["10.0.1.0/24", "10.0.17.0/24", "10.0.18.0/24"]
   subnet_names    = ["sn1-${module.network.vnet_name}", "sn2-${module.network.vnet_name}", "sn3-${module.network.vnet_name}"] //sn1-vnet-ldo-euw-dev-01
   subnet_service_endpoints = {
     "sn1-${module.network.vnet_name}" = ["Microsoft.Storage"]                   // Adds extra subnet endpoints to sn1-vnet-ldo-euw-dev-01
     "sn2-${module.network.vnet_name}" = ["Microsoft.Storage", "Microsoft.Sql"], // Adds extra subnet endpoints to sn2-vnet-ldo-euw-dev-01
     "sn3-${module.network.vnet_name}" = ["Microsoft.AzureActiveDirectory"]      // Adds extra subnet endpoints to sn3-vnet-ldo-euw-dev-01
+  }
+
+  subnet_delegation = {
+
+    "sn2-${module.network.vnet_name}" = {
+      "Microsoft.Network/dnsResolvers" = {
+        service_name    = "Microsoft.Network/dnsResolvers"
+        service_actions = "Microsoft.Network/virtualNetworks/subnets/join/action"
+      }
+    }
+
+    "sn3-${module.network.vnet_name}" = {
+      "Microsoft.Network/dnsResolvers" = {}
+      service_name                     = "Microsoft.Network/dnsResolvers"
+      service_actions                  = "Microsoft.Network/virtualNetworks/subnets/join/action"
+    }
   }
 }
